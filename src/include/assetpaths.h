b67fdbf059e7fcb9b416efe6e53f44b2a2b7ca3e
Orchid <lokipokes@hotmail.com>
2025-08-09 11:04:17 +0100

    initial commit, got text rendering.

---
+++ src/include/assetpaths.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+// all written as relative from the base dir.
+
+inline const char* arialFontPath = "assets/fonts/arial.ttf";
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+#include "assetpaths.h"
+
+std::string getAssetPath(const std::string& relativePath)
+{
+    std::filesystem::path exePath = std::filesystem::current_path();
+    return(exePath / relativePath).string();
+}
 
 int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+{   
+    sf::Font defaultFont;
+    if (!defaultFont.openFromFile(getAssetPath(arialFontPath))) {
+        std::cerr << "failed to load font from" << std::endl;
+    }
+
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "TopDownShooter");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
+        sf::Text text(defaultFont, "TopDownShooter", 25);
+
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -14,8 +30,14 @@ int main()
                 window.close();
             }
         }
+        
 
         window.clear();
+		
+        //draw here
+        window.draw(text);
+        
+        //finalise
         window.display();
     }
 }
