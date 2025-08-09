37525a820103466539b4a870973d88d785c927dc
Orchid <lokipokes@hotmail.com>
2025-08-09 11:20:41 +0100

    basic movement and player.

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,61 @@ std::string getAssetPath(const std::string& relativePath)
 
 int main()
 {   
+    // assets
     sf::Font defaultFont;
     if (!defaultFont.openFromFile(getAssetPath(arialFontPath))) {
-        std::cerr << "failed to load font from" << std::endl;
+        std::cerr << "failed to load font arial" << std::endl;
     }
 
+    /*
+        game objects
+    */
+
+    //player
+    sf::CircleShape playerSprite(50.f);
+
+    // window
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "TopDownShooter");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
-        sf::Text text(defaultFont, "TopDownShooter", 25);
-
         while (const std::optional event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
-            {
+            // close window.
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }   
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())    {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
             }
-        }
-        
+
+            // input handling
+            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scan::W)
+                    playerSprite.move(sf::Vector2f(0.0f, -5.0f));
+
+                if (keyPressed->scancode == sf::Keyboard::Scan::A)
+                    playerSprite.move(sf::Vector2f(-5.0f, 0.0f));
+               
+                if (keyPressed->scancode == sf::Keyboard::Scan::D)
+                    playerSprite.move(sf::Vector2f(5.0f, 0.0f));
+
+                if (keyPressed->scancode == sf::Keyboard::Scan::S)
+                    playerSprite.move(sf::Vector2f(0.0f, 5.0f));
+            }
+        } 
+
+        sf::Text text(defaultFont, "TopDownShooter", 25);
 
         window.clear();
 		
         //draw here
         window.draw(text);
+
+        window.draw(playerSprite);
         
         //finalise
         window.display();
